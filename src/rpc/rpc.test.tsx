e7f05428a23a51702a9e560c688ca261e04c95e1
Goh Yong Jie <gohyongjie1112@gmail.com>
2024-03-20 22:37:50 +0800

    feat: add decode message and test

--- src/rpc/rpc.test.tsx
+++ src/rpc/rpc.test.tsx
@@ -1,13 +1,20 @@
 import { expect, test } from "bun:test";
-import { encodeMessage } from "./rpc";
+import { decodeMessage, encodeMessage } from "./rpc";
 
 type EncodeTest = {
   testing: boolean;
 }
 
-test("Encode message", () => {
+test("Test encode", () => {
   const expected = "Content-Length: 16\r\n\r\n{\"testing\":true}";
   const encodeTest: EncodeTest = { testing: true }
   const actual = encodeMessage(encodeTest);
   expect(actual).toBe(expected);
+})
+
+test("Test decode", () => {
+  const incomingMessage = "Content-Length: 17\r\n\r\n{\"Method\":\"test\"}";
+  const decodedMessage = decodeMessage(incomingMessage);
+  expect(decodedMessage.method).toBe("test");
+  expect(decodedMessage.length).toBe(17);
 })
